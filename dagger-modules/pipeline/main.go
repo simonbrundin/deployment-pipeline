954fc66b5f5e867b774b49e0444445889d2ccb04
Simon Brundin <simonbrundin@gmail.com>
2025-09-09 23:31:48 +0200

    Buildkit istället för Docker som är default

--- dagger-modules/pipeline/main.go
+++ dagger-modules/pipeline/main.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"dagger/pipeline/internal/dagger"
+	"os"
 )
 
 type Pipeline struct{}
 
-// CI kör fullt CI-workflow
+// CI kör komplett CI-workflow
 func (m *Pipeline) CI(stringArg string) *dagger.Container {
+	// Använd Buildkit som byggmotor istället för Docker. Buildkit kan till
+	// skillnad från Docker bygga både lokalt och i Kubernetes.
+	os.Setenv("DAGGER_ENGINE_BACKEND", "buildkit")
+
 	// 1. Kör unit tests
 	containerWithTests := m.UnitTests(stringArg)
 
